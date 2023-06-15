7d844ad9331c70446fe2e6c7522493ea85846d4b
RenanaYehuda <66746638+RenanaYehuda@users.noreply.github.com>
2023-06-15 09:48:30 +0300

    av

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import "./App.css";
-import AppHooks from "./hooks_comps/appHooks";
 import React from "react";
-import Toggle from "./hooks_comps/toggle";
-import SelectColor from "./hooks_comps/selectColor";
-import AppTv from "./api_comps/appTv";
 import AppHw from "./comps/appHw";
 
 function App() {
   return (
     <div className="container">
-      {/* <AppHw /> */}
-      {/* <AppHooks />
-      <Toggle />
-      <SelectColor /> */}
+      <AppHw />
     </div>
   );
 }
---
+++ src/comps_hw/kazino.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+export default function Kazino() {
+    const [role, setRole] = useState(5);
+    const [coin, setCoin] = useState(5);
+
+    const scrole = () => {
+        debugger
+       if(coin>0){
+        setCoin(coin-1);
+        let rnd = Math.random()*6;
+        setRole(Math.ceil(rnd));
+        if(coin==5||coin==6){
+        setCoin(coin+2);
+       }
+    }
+       else{
+        alert("you need more money and luck ....")
+       }
+        
+        
+    }
+
+    return (
+        <>
+        <div className="container">
+            <img src={`../images/dice_images/dice${role}.jpg`} alt={role.toString()}/>
+            <hr></hr>
+            <button className="btn btn-primary btn-rounded" onClick={scrole}>scrole</button>
+          <h3>your coins: {coin}</h3>
+            </div>
+        </>
+    )
+}
